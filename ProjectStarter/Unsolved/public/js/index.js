f1ce488a6bcbeb665b51e924721f445cc283ddd5
Lisa Johnson <lmaejo@gmail.com>
2018-08-23 15:03:53 -0500

    first commit

---
+++ ProjectStarter/Unsolved/public/js/index.js
@@ -0,0 +1,152 @@
+// Get references to page elements
+// var $exampleText = $("#example-text");
+// var $exampleDescription = $("#example-description");
+// var $submitBtn = $("#submit");
+// var $exampleList = $("#example-list");
+var i = 0;
+$( document ).ready(function() {
+  // The API object contains methods for each kind of request we'll make
+  const triviaClient = {
+    getTriviaQuestions: (params) => {
+      return $.ajax({
+        type: "GET",
+        url: "https://opentdb.com/api.php?amount=10"
+        // data: "Questions"
+        // data: JSON.stringify(triviaClient)
+      })
+    }
+  };
+
+  triviaClient.getTriviaQuestions().then(data => dispQuestions(data));
+  
+  function dispQuestions(data) {
+    console.log(data)
+    $('.box').html(data.results.map(result => result.question))
+    $('#question-text').val(data.results[i].question);
+
+
+    $('#select1')
+    .find('option')
+    .remove()
+    .end();
+    $('#select2')
+    .find('option')
+    .remove()
+    .end();
+
+    $('#select1').append($('<option>', {value:data.results[i].correct_answer, text:data.results[i].correct_answer}));
+    data.results[i].incorrect_answers.forEach(function(answer) {
+      $('#select1').append($('<option>', {value:answer, text:answer}));
+    });
+    $('#select2').append($('<option>', {value:data.results[i].correct_answer, text:data.results[i].correct_answer}));
+    data.results[i].incorrect_answers.forEach(function(answer) {
+      $('#select2').append($('<option>', {value:answer, text:answer}));
+    });
+
+    i++;
+    }
+  
+   $("#submit").on("click", function(e) {
+    e.preventDefault()
+    triviaClient.getTriviaQuestions().then(data => dispQuestions(data))
+  
+    console.log(triviaClient)
+   }) 
+     
+    });
+  
+  
+  
+  
+  // var API = {
+  //   triviaClient: function(questions) {
+  //     return $.ajax({
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       type: "POST",
+  //       url: "api/examples",
+  //       data: JSON.stringify(questions)
+  //     });
+  //   },
+  //   getExamples: function() {
+  //     return $.ajax({
+  //       url: "api/examples",
+  //       type: "GET"
+  //     });
+  //   },
+  //   deleteExample: function(id) {
+  //     return $.ajax({
+  //       url: "api/examples/" + id,
+  //       type: "DELETE"
+  //     });
+  //   }
+  // };
+  
+  // refreshExamples gets new examples from the db and repopulates the list
+  // var refreshExamples = function() {
+  //   API.getExamples().then(function(data) {
+  //     var $examples = data.map(function(example) {
+  //       var $a = $("<a>")
+  //         .text(example.text)
+  //         .attr("href", "/example/" + example.id);
+  
+  //       var $li = $("<li>")
+  //         .attr({
+  //           class: "list-group-item",
+  //           "data-id": example.id
+  //         })
+  //         .append($a);
+  
+  //       var $button = $("<button>")
+  //         .addClass("btn btn-danger float-right delete")
+  //         .text("ｘ");
+  
+  //       $li.append($button);
+  
+  //       return $li;
+  //     });
+  
+  //     $exampleList.empty();
+  //     $exampleList.append($examples);
+  //   });
+  // };
+  
+  // // handleFormSubmit is called whenever we submit a new example
+  // // Save the new example to the db and refresh the list
+  // var handleFormSubmit = function(event) {
+  //   event.preventDefault();
+  
+  //   var example = {
+  //     text: $exampleText.val().trim(),
+  //     description: $exampleDescription.val().trim()
+  //   };
+  
+  //   if (!(example.text && example.description)) {
+  //     alert("You must enter an example text and description!");
+  //     return;
+  //   }
+  
+  //   API.saveExample(example).then(function() {
+  //     refreshExamples();
+  //   });
+  
+  //   $exampleText.val("");
+  //   $exampleDescription.val("");
+  // };
+  
+  // // handleDeleteBtnClick is called when an example's delete button is clicked
+  // // Remove the example from the db and refresh the list
+  // var handleDeleteBtnClick = function() {
+  //   var idToDelete = $(this)
+  //     .parent()
+  //     .attr("data-id");
+  
+  //   API.deleteExample(idToDelete).then(function() {
+  //     refreshExamples();
+  //   });
+  // };
+  
+  // // Add event listeners to the submit and delete buttons
+  // $submitBtn.on("click", handleFormSubmit);
+  // $exampleList.on("click", ".delete", handleDeleteBtnClick);
